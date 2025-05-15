e6e6753db8b15b0344f2c15a6ec69f4c6170b357
Mohamed <meradmohamedsaid@gmail.com>
2025-05-15 15:54:58 +0100

    finalS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import TspGa from "./Pages/TspGa";
+
 import Sudoku from "./Pages/Sudoko";
 import Home from "./Pages/Home";
-import TspGa2 from "./Pages/TspGA2";
+
 import TpGATest from "./Pages/TpGATest";
 function App() {
   const [count, setCount] = useState(0);
